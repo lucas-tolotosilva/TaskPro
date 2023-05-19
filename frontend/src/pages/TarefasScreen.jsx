68c98e3a15285d0fa3c7d229547f234475580485
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2023-05-18 21:31:15 -0300

    Create the table on the tarefaScreen

--- frontend/src/pages/TarefasScreen.jsx
+++ frontend/src/pages/TarefasScreen.jsx
@@ -20,18 +20,20 @@ export function TarefasScreen() {
         <div className='bg-[#EDF2E3] min-h-screen'>
             <Header />
             <div className='w-full h-full flex justify-center mt-16'>
-                <table className='table-auto w-3/4 text-left border  border-sky-500'>
-                    <thead className='bg-[#C0D29D]'>
-                        <th>Prioridade</th>
-                        <th>Tarefa</th>
-                        <th>Descricao</th>
-                        <th>Criador</th>
-                        <th>Status</th>
+                <table className='table-auto w-3/4 text-left rounded-lg'>
+                    <thead className='bg-[#C0D29D] px-10'>
+                        <tr className=''>
+                            <th className='py-3 pl-3 rounded-tl-lg'>Prioridade</th>
+                            <th>Tarefa</th>
+                            <th>Descricao</th>
+                            <th>Criador</th>
+                            <th className='rounded-tr-lg'>Status</th>
+                        </tr>
                     </thead>
                     <tbody>
                         {tarefas && Object.values(tarefas).map(tarefa => (
-                            <tr key={tarefa.idTarefa}>
-                                <td>{tarefa.nome}</td>
+                            <tr key={tarefa.idTarefa} className={`${tarefa.idTarefa % 2 === 0 ? 'bg-[#dfe7cf]' : ''} border-b-2 mx-3 rounded-lg`}>
+                                <td className='py-3 pl-3 '>{tarefa.nome}</td>
                                 <td>{tarefa.descricao}</td>
                                 <td>{tarefa.status}</td>
                                 <td>{tarefa.nomeUsuario}</td>
