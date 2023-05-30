cbb1feb38d861c52900baab7e65176db47dfbf32
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2023-05-30 20:59:56 -0300

    Fixing the order by tag funcionality

--- frontend/src/pages/TarefasScreen.jsx
+++ frontend/src/pages/TarefasScreen.jsx
@@ -15,7 +15,9 @@ export function TarefasScreen() {
     const [modalIsOpen, setIsOpen] = useState(false);
     const [modalUpdateIsOpen, setUpdateIsOpen] = useState(false);
     const [selectedTask, setSelectedTask] = useState();
+    const [orderTarefas, setOrderTarefas] = useState();
     const [orderValue, setOrderValue] = useState();
+    const [orderTag, setOrderTag] = useState();
 
     const openModal = () => {
         setIsOpen(true)
@@ -48,10 +50,32 @@ export function TarefasScreen() {
         setOrderValue(e.target.value)
     }
 
+    console.log(orderValue)
+
+    const handleOrderTag = (e) => {
+        setOrderTag(e.target.value)
+    }
+
+    useEffect(() => {
+        let orderedTasks = [];
+
+        if (orderTag === 'Alta Urgência') {
+            orderedTasks = tarefas.filter(tarefa => tarefa.nomeTag === 'Alta Urgência');
+        } else if (orderTag === 'Média Urgência') {
+            orderedTasks = tarefas.filter(tarefa => tarefa.nomeTag === 'Média Urgência');
+        } else if (orderTag === 'Baixa Urgência') {
+            orderedTasks = tarefas.filter(tarefa => tarefa.nomeTag === 'Baixa Urgência');
+        } else {
+            orderedTasks = tarefas; // Caso nenhuma opção seja selecionada, mantenha a ordem original
+        }
+
+        setOrderTarefas(orderedTasks);
+    }, [orderTag, tarefas]);
+
     useEffect(() => {
         dispatch(listTarefas())
     }, [dispatch])
-    console.log(orderValue)
+
     return (
         <div className='bg-[#EDF2E3] min-h-screen'>
             <Header />
@@ -60,12 +84,26 @@ export function TarefasScreen() {
                     <button onClick={openModal} className='border-2 p-2 rounded-full flex items-center gap-3 border-[#408d89]'>Criar Nova Tarefa <img className='w-5' src={iAdd} /></button>
                     <label>Ordenar por: </label>
                     <select onChange={handleOrder} value={orderValue} className='p-1 rounded-lg text-gray'>
+                        <option className='text-black'>Todos</option>
                         <option className='text-black'>Tag</option>
                         <option className='text-black'>Status</option>
                         <option className='text-black'>Categoria</option>
                     </select>
                 </div>
+
                 <CreateTask closeModal={closeModal} modalIsOpen={modalIsOpen} />
+
+                {orderValue === 'Tag' ?
+                    <div className='w-3/4 justify-start mt-10'>
+                        <label>Escolha o nível de Urgência: </label>
+                        <select onChange={handleOrderTag} value={orderTag} className='p-1 rounded-lg text-gray'>
+                            <option value="Alta Urgência" className='text-black'>Alta Urgência</option>
+                            <option value="Média Urgência" className='text-black'>Média Urgência</option>
+                            <option value="Baixa Urgência" className='text-black'>Baixa Urgência</option>
+                        </select>
+                    </div> : null
+
+                }
 
 
                 <table className='table-auto w-3/4 text-left rounded-lg'>
@@ -81,7 +119,8 @@ export function TarefasScreen() {
                         </tr>
                     </thead>
                     <tbody>
-                        {tarefas && Object.values(tarefas).map((tarefa, index) => (
+
+                        {orderTarefas && Object.values(orderTarefas).map((tarefa, index) => (
                             <tr key={index} className={`${index % 2 === 0 ? 'bg-[#dfe7cf]' : ''} border-b-2 mx-3 rounded-lg`}>
                                 <td className='py-3 pl-3 '>{tarefa.nomeTag}</td>
                                 <td >{tarefa.nome}</td>
