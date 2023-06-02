bfcafd0b9345dd5e5978ba5ee9dde45a555e7ee5
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2023-06-02 14:50:10 -0300

    Update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -183,8 +183,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_ROOT = BASE_DIR 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
